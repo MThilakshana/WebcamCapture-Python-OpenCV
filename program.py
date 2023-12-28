5abfbac8231588e7f8765dcd4460c4278437b88a
MThilakshana <madurathilakshana999@gmail.com>
2023-12-28 09:14:15 +0530

    28/12

---
+++ program.py
@@ -0,0 +1,23 @@
+import cv2
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("Test")
+img_counter = 0
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+    cv2.imshow("test",frame)
+    
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        print("Closing...")
+        break
+    if k%256 == 32:
+        img_name = "OpenCV{}.png".format(img_counter)
+        cv2.imwrite(img_name,frame)
+        print("{} written!".format(img_name))
+        img_counter = img_counter + 1
+        
+cam.release()
+cv2.destroyAllWindows()
